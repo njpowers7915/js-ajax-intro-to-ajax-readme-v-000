784b2d257854d07022dcfa8088c88360123188c7
Nicholas Powers <njpowers7915@gmail.com>
2018-08-06 18:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories() {
  const req = new XMLHttpRequest()
- req.open("GET", 'https://api.github.com/users/octocat/repos')
+ req.open("GET", 'https://api.github.com/users/njpowers7915/repos')
  req.send()
 }
